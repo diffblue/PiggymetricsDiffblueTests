61a5b9f3329f638370173f5ac13506a136bbf22f
Diffblue CI <33348585+db-ci-platform@users.noreply.github.com>
2025-12-03 15:39:32 +0000

    Update tests for Diffblue Cover 2025.12.01

--- account-service/src/test/java/com/piggymetrics/account/config/ResourceServerConfigDiffblueTest.java
+++ account-service/src/test/java/com/piggymetrics/account/config/ResourceServerConfigDiffblueTest.java
@@ -10,33 +10,17 @@ import com.diffblue.cover.annotations.MethodsUnderTest;
 import com.piggymetrics.account.service.security.CustomUserInfoTokenServices;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorHandler;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.common.AuthenticationScheme;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
-@ContextConfiguration(
-    classes = {
-      ResourceServerConfig.class,
-      ResourceServerProperties.class,
-      AuthenticationConfiguration.class
-    })
-@RunWith(SpringJUnit4ClassRunner.class)
 public class ResourceServerConfigDiffblueTest {
-  @Autowired private ResourceServerConfig resourceServerConfig;
-
-  @Autowired private ResourceServerProperties resourceServerProperties;
-
   /**
    * Test {@link ResourceServerConfig#clientCredentialsResourceDetails()}.
    *
@@ -52,45 +36,6 @@ public class ResourceServerConfigDiffblueTest {
     // Arrange and Act
     ClientCredentialsResourceDetails actualClientCredentialsResourceDetailsResult =
         new ResourceServerConfig(new ResourceServerProperties()).clientCredentialsResourceDetails();
-
-    // Assert
-    assertEquals("access_token", actualClientCredentialsResourceDetailsResult.getTokenName());
-    assertEquals("client_credentials", actualClientCredentialsResourceDetailsResult.getGrantType());
-    assertNull(actualClientCredentialsResourceDetailsResult.getAccessTokenUri());
-    assertNull(actualClientCredentialsResourceDetailsResult.getClientId());
-    assertNull(actualClientCredentialsResourceDetailsResult.getClientSecret());
-    assertNull(actualClientCredentialsResourceDetailsResult.getId());
-    assertNull(actualClientCredentialsResourceDetailsResult.getScope());
-    assertEquals(
-        AuthenticationScheme.header,
-        actualClientCredentialsResourceDetailsResult.getAuthenticationScheme());
-    assertEquals(
-        AuthenticationScheme.header,
-        actualClientCredentialsResourceDetailsResult.getClientAuthenticationScheme());
-    assertFalse(actualClientCredentialsResourceDetailsResult.isAuthenticationRequired());
-    assertFalse(actualClientCredentialsResourceDetailsResult.isScoped());
-    assertTrue(actualClientCredentialsResourceDetailsResult.isClientOnly());
-  }
-
-  /**
-   * Test {@link ResourceServerConfig#clientCredentialsResourceDetails()}.
-   *
-   * <ul>
-   *   <li>Given {@link ResourceServerConfig}.
-   * </ul>
-   *
-   * <p>Method under test: {@link ResourceServerConfig#clientCredentialsResourceDetails()}
-   */
-  @Test
-  @Category(ContributionFromDiffblue.class)
-  @ManagedByDiffblue
-  @MethodsUnderTest({
-    "ClientCredentialsResourceDetails ResourceServerConfig.clientCredentialsResourceDetails()"
-  })
-  public void testClientCredentialsResourceDetails_givenResourceServerConfig() {
-    // Arrange and Act
-    ClientCredentialsResourceDetails actualClientCredentialsResourceDetailsResult =
-        resourceServerConfig.clientCredentialsResourceDetails();
 
     // Assert
     assertEquals("access_token", actualClientCredentialsResourceDetailsResult.getTokenName());
@@ -130,28 +75,6 @@ public class ResourceServerConfigDiffblueTest {
   }
 
   /**
-   * Test {@link ResourceServerConfig#oauth2FeignRequestInterceptor()}.
-   *
-   * <ul>
-   *   <li>Then return {@link OAuth2FeignRequestInterceptor}.
-   * </ul>
-   *
-   * <p>Method under test: {@link ResourceServerConfig#oauth2FeignRequestInterceptor()}
-   */
-  @Test
-  @Category(ContributionFromDiffblue.class)
-  @ManagedByDiffblue
-  @MethodsUnderTest({
-    "feign.RequestInterceptor ResourceServerConfig.oauth2FeignRequestInterceptor()"
-  })
-  public void testOauth2FeignRequestInterceptor_thenReturnOAuth2FeignRequestInterceptor() {
-    // Arrange, Act and Assert
-    assertTrue(
-        resourceServerConfig.oauth2FeignRequestInterceptor()
-            instanceof OAuth2FeignRequestInterceptor);
-  }
-
-  /**
    * Test {@link ResourceServerConfig#clientCredentialsRestTemplate()}.
    *
    * <p>Method under test: {@link ResourceServerConfig#clientCredentialsRestTemplate()}
@@ -182,63 +105,6 @@ public class ResourceServerConfigDiffblueTest {
             instanceof DefaultUriBuilderFactory);
     assertEquals(7, actualClientCredentialsRestTemplateResult.getMessageConverters().size());
     assertTrue(actualClientCredentialsRestTemplateResult.getInterceptors().isEmpty());
-  }
-
-  /**
-   * Test {@link ResourceServerConfig#clientCredentialsRestTemplate()}.
-   *
-   * <ul>
-   *   <li>Given {@link ResourceServerConfig}.
-   * </ul>
-   *
-   * <p>Method under test: {@link ResourceServerConfig#clientCredentialsRestTemplate()}
-   */
-  @Test
-  @Category(ContributionFromDiffblue.class)
-  @ManagedByDiffblue
-  @MethodsUnderTest({"OAuth2RestTemplate ResourceServerConfig.clientCredentialsRestTemplate()"})
-  public void testClientCredentialsRestTemplate_givenResourceServerConfig() {
-    // Arrange and Act
-    OAuth2RestTemplate actualClientCredentialsRestTemplateResult =
-        resourceServerConfig.clientCredentialsRestTemplate();
-
-    // Assert
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getRequestFactory()
-            instanceof SimpleClientHttpRequestFactory);
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getOAuth2ClientContext()
-            instanceof DefaultOAuth2ClientContext);
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getErrorHandler() instanceof OAuth2ErrorHandler);
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getResource()
-            instanceof ClientCredentialsResourceDetails);
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getUriTemplateHandler()
-            instanceof DefaultUriBuilderFactory);
-    assertEquals(7, actualClientCredentialsRestTemplateResult.getMessageConverters().size());
-    assertTrue(actualClientCredentialsRestTemplateResult.getInterceptors().isEmpty());
-  }
-
-  /**
-   * Test {@link ResourceServerConfig#tokenServices()}.
-   *
-   * <ul>
-   *   <li>Given {@link ResourceServerConfig}.
-   * </ul>
-   *
-   * <p>Method under test: {@link ResourceServerConfig#tokenServices()}
-   */
-  @Test
-  @Category(ContributionFromDiffblue.class)
-  @ManagedByDiffblue
-  @MethodsUnderTest({
-    "org.springframework.security.oauth2.provider.token.ResourceServerTokenServices ResourceServerConfig.tokenServices()"
-  })
-  public void testTokenServices_givenResourceServerConfig() {
-    // Arrange, Act and Assert
-    assertTrue(resourceServerConfig.tokenServices() instanceof CustomUserInfoTokenServices);
   }
 
   /**
--- notification-service/src/test/java/com/piggymetrics/notification/config/ResourceServerConfigDiffblueTest.java
+++ notification-service/src/test/java/com/piggymetrics/notification/config/ResourceServerConfigDiffblueTest.java
@@ -9,64 +9,16 @@ import com.diffblue.cover.annotations.ManagedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorHandler;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.common.AuthenticationScheme;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
-@ContextConfiguration(classes = {ResourceServerConfig.class, AuthenticationConfiguration.class})
-@RunWith(SpringJUnit4ClassRunner.class)
 public class ResourceServerConfigDiffblueTest {
-  @Autowired private ResourceServerConfig resourceServerConfig;
-
-  /**
-   * Test {@link ResourceServerConfig#clientCredentialsResourceDetails()}.
-   *
-   * <ul>
-   *   <li>Given {@link ResourceServerConfig}.
-   * </ul>
-   *
-   * <p>Method under test: {@link ResourceServerConfig#clientCredentialsResourceDetails()}
-   */
-  @Test
-  @Category(ContributionFromDiffblue.class)
-  @ManagedByDiffblue
-  @MethodsUnderTest({
-    "ClientCredentialsResourceDetails ResourceServerConfig.clientCredentialsResourceDetails()"
-  })
-  public void testClientCredentialsResourceDetails_givenResourceServerConfig() {
-    // Arrange and Act
-    ClientCredentialsResourceDetails actualClientCredentialsResourceDetailsResult =
-        resourceServerConfig.clientCredentialsResourceDetails();
-
-    // Assert
-    assertEquals("access_token", actualClientCredentialsResourceDetailsResult.getTokenName());
-    assertEquals("client_credentials", actualClientCredentialsResourceDetailsResult.getGrantType());
-    assertNull(actualClientCredentialsResourceDetailsResult.getAccessTokenUri());
-    assertNull(actualClientCredentialsResourceDetailsResult.getClientId());
-    assertNull(actualClientCredentialsResourceDetailsResult.getClientSecret());
-    assertNull(actualClientCredentialsResourceDetailsResult.getId());
-    assertNull(actualClientCredentialsResourceDetailsResult.getScope());
-    assertEquals(
-        AuthenticationScheme.header,
-        actualClientCredentialsResourceDetailsResult.getAuthenticationScheme());
-    assertEquals(
-        AuthenticationScheme.header,
-        actualClientCredentialsResourceDetailsResult.getClientAuthenticationScheme());
-    assertFalse(actualClientCredentialsResourceDetailsResult.isAuthenticationRequired());
-    assertFalse(actualClientCredentialsResourceDetailsResult.isScoped());
-    assertTrue(actualClientCredentialsResourceDetailsResult.isClientOnly());
-  }
-
   /**
    * Test {@link ResourceServerConfig#clientCredentialsResourceDetails()}.
    *
@@ -82,7 +34,7 @@ public class ResourceServerConfigDiffblueTest {
   @MethodsUnderTest({
     "ClientCredentialsResourceDetails ResourceServerConfig.clientCredentialsResourceDetails()"
   })
-  public void testClientCredentialsResourceDetails_givenResourceServerConfig2() {
+  public void testClientCredentialsResourceDetails_givenResourceServerConfig() {
     // Arrange and Act
     ClientCredentialsResourceDetails actualClientCredentialsResourceDetailsResult =
         new ResourceServerConfig().clientCredentialsResourceDetails();
@@ -129,65 +81,6 @@ public class ResourceServerConfigDiffblueTest {
   }
 
   /**
-   * Test {@link ResourceServerConfig#oauth2FeignRequestInterceptor()}.
-   *
-   * <ul>
-   *   <li>Then return {@link OAuth2FeignRequestInterceptor}.
-   * </ul>
-   *
-   * <p>Method under test: {@link ResourceServerConfig#oauth2FeignRequestInterceptor()}
-   */
-  @Test
-  @Category(ContributionFromDiffblue.class)
-  @ManagedByDiffblue
-  @MethodsUnderTest({
-    "feign.RequestInterceptor ResourceServerConfig.oauth2FeignRequestInterceptor()"
-  })
-  public void testOauth2FeignRequestInterceptor_thenReturnOAuth2FeignRequestInterceptor() {
-    // Arrange, Act and Assert
-    assertTrue(
-        resourceServerConfig.oauth2FeignRequestInterceptor()
-            instanceof OAuth2FeignRequestInterceptor);
-  }
-
-  /**
-   * Test {@link ResourceServerConfig#clientCredentialsRestTemplate()}.
-   *
-   * <ul>
-   *   <li>Given {@link ResourceServerConfig}.
-   * </ul>
-   *
-   * <p>Method under test: {@link ResourceServerConfig#clientCredentialsRestTemplate()}
-   */
-  @Test
-  @Category(ContributionFromDiffblue.class)
-  @ManagedByDiffblue
-  @MethodsUnderTest({"OAuth2RestTemplate ResourceServerConfig.clientCredentialsRestTemplate()"})
-  public void testClientCredentialsRestTemplate_givenResourceServerConfig() {
-    // Arrange and Act
-    OAuth2RestTemplate actualClientCredentialsRestTemplateResult =
-        resourceServerConfig.clientCredentialsRestTemplate();
-
-    // Assert
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getRequestFactory()
-            instanceof SimpleClientHttpRequestFactory);
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getOAuth2ClientContext()
-            instanceof DefaultOAuth2ClientContext);
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getErrorHandler() instanceof OAuth2ErrorHandler);
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getResource()
-            instanceof ClientCredentialsResourceDetails);
-    assertTrue(
-        actualClientCredentialsRestTemplateResult.getUriTemplateHandler()
-            instanceof DefaultUriBuilderFactory);
-    assertEquals(7, actualClientCredentialsRestTemplateResult.getMessageConverters().size());
-    assertTrue(actualClientCredentialsRestTemplateResult.getInterceptors().isEmpty());
-  }
-
-  /**
    * Test {@link ResourceServerConfig#clientCredentialsRestTemplate()}.
    *
    * <ul>
@@ -200,7 +93,7 @@ public class ResourceServerConfigDiffblueTest {
   @Category(ContributionFromDiffblue.class)
   @ManagedByDiffblue
   @MethodsUnderTest({"OAuth2RestTemplate ResourceServerConfig.clientCredentialsRestTemplate()"})
-  public void testClientCredentialsRestTemplate_givenResourceServerConfig2() {
+  public void testClientCredentialsRestTemplate_givenResourceServerConfig() {
     // Arrange and Act
     OAuth2RestTemplate actualClientCredentialsRestTemplateResult =
         new ResourceServerConfig().clientCredentialsRestTemplate();
